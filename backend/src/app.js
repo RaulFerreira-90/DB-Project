4cc66038c19ffe5acd3f456402468bcc7a62e8e7
Raul Oliveira Ferreira <raul.oliveiraferreira@gmail.com>
2020-03-28 14:19:54 -0300

    Estudo de testes da aplicação

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,15 @@
 const express = require('express')
-const cors = require('cors'); 
+const cors = require('cors')
 const routes = require('./routes')
-
+const { errors } = require('celebrate')
 const app = express()
 
 app.use(cors()) /* está 'app.use(cors())', porque estamos em desenv, caso fosse para produção seria "app.use(cors({ origin: 'url de hospedagem' }))" */
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
-app.listen(3333)
+module.exports = app
 
 /**
  * Rota / Recurso
